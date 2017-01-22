2c96bac1b9575cb0320787f7ceb4b80e0657f7a6
tony77177 <tony77177@gmail.com>
2017-01-23 00:15:37 +0800

    删除注释信息

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -6,7 +6,6 @@
     <div class="text-center muted">
         &copy; <script>document.write((new Date().getFullYear()))</script>
         <?php echo $this->config->config['sys_name'].'&nbsp;'.$this->config->config['sys_version'];?> <br/>
-        <!--        Powered by <a href="http://www.freedomdream.cn" target="_blank">畅想科技</a>-->
     </div>
 </footer>
 
